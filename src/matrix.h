202a9c567f8f82633c3d5e795880b3e33ca1d88d
Felipe Quaresma Vieira <135285820+felipeqvieira@users.noreply.github.com>
2023-09-05 21:00:30 -0300

    Criei o readme e comentei as funções

--- src/matrix.h
+++ src/matrix.h
@@ -16,13 +16,32 @@ typedef struct
   Vector v;
 } LinearEquation;
 
+// printa a matriz
 void printMatrix(Matrix m);
+
+// printa o vetor de termos independentes
 void printVector(Vector v);
+
+// le uma equacao linear
 void readLinearEquation(Matrix *m, Vector *v);
+
+// printa a equacao linear 
 void printLinearEquation(Matrix m, Vector v);
+
+// executa a eliminacao de gauss sem pivoteamento
 void gaussEliminationWithPivoting(Matrix *m, Vector *c);
+
+// printa a solucao depois da retrossubstituicao
 Vector *printSolutionBySubstitution(Matrix m, Vector c);
+
+// printa o residuo
 void printResidual(Matrix m, Vector c, Vector solution);
+
+// copia a equacao linear
 LinearEquation copyLinearEquation(LinearEquation la);
+
+// executa a eliminacao de gauss com pivoteamento, mas sem os multiplicadores
 void gaussEliminationWithPivotingWithoutMult(Matrix *m, Vector *c);
+
+// executa a eliminacao de gauss sem pivoteamento
 void gaussEliminationWithoutPivoting(Matrix *m, Vector *c);
